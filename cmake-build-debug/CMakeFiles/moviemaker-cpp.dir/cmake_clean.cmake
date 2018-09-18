0a9ce846e697bedeb90ecddab2db7fb957be4a86
hyangwinter <824141161@qq.com>
2018-09-18 14:49:59 +0800

    new local repository

---
+++ cmake-build-debug/CMakeFiles/moviemaker-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moviemaker-cpp.dir/src/test.cpp.o"
+  "CMakeFiles/moviemaker-cpp.dir/src/reader.cpp.o"
+  "CMakeFiles/moviemaker-cpp.dir/src/writer.cpp.o"
+  "moviemaker-cpp.pdb"
+  "moviemaker-cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moviemaker-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
